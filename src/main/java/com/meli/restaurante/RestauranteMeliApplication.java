f1cc35cef1386ce5ce00bd4deb575c8273a4f876
Lucca Gabriel Dias Barbosa <lucca.dias@mercadolivre.com>
2021-12-27 11:28:06 -0300

    Primeiro commit

---
+++ src/main/java/com/meli/restaurante/RestauranteMeliApplication.java
@@ -0,0 +1,13 @@
+package com.meli.restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteMeliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestauranteMeliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meli/restaurante/RestauranteMeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteMeliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
